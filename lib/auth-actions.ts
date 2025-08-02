9633e1887dbd1e9fbd131b6c7282efe0163d02a8
anmol singh <166398513+Anmol126singh@users.noreply.github.com>
2025-08-02 14:33:10 -0400

    Update auth-actions.ts

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -3,7 +3,7 @@
 import { signIn, signOut } from "@/auth";
 
 export const login = async () => {
-  await signIn("github",, { redirectTo: "/" });  // <-- no redirectTo here
+  await signIn("github", { redirectTo: "/" });  // <-- no redirectTo here
 };
 
 export const logout = async () => {
